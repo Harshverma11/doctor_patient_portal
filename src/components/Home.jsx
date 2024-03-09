8409d16fa04933cec2480cfe55d1fdd8b8ccc05f
harsh verma <harshverma16172@gamil.com>
2024-03-09 20:05:55 +0530

    modificatiom in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,101 @@
 import React from 'react'
+import {Link,NavLink} from "react-router-dom" 
 
-function Home() {
+
+  function Home() {
   return (
-    <div>Home
-      <h1>Wellcone to our webSite</h1>
-      <h2>gtvyt</h2>
+    <>
+    <div className='grid gap-1 m-1 sm:grid-cols-3 grid-cols-1'  >
+      <div className="rounded-md bg-orange-400 min-h-12 text-center">
+         <Link
+             to="#"
+             
+          >
+            1
+         </Link>
+         </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center">
+      <Link
+             to="#"
+             
+          >
+            2
+         </Link>
+      </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center" >
+      <Link
+             to="#"
+             
+          >
+            3
+         </Link>
+      </div>
+
+     
     </div>
+
+    <div className='grid gap-1 m-1 sm:grid-cols-3 grid-cols-1'  >
+      <div className="rounded-md bg-orange-400 min-h-12 text-center">
+         <Link
+             to="#"
+             
+          >
+            4
+         </Link>
+         </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center">
+      <Link
+             to="#"
+             
+          >
+            5
+         </Link>
+      </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center" >
+      <Link
+             to="#"
+             
+          >
+            6
+         </Link>
+      </div>
+
+     
+    </div>
+    <div className='grid gap-1 m-1 sm:grid-cols-3 grid-cols-1'  >
+      <div className="rounded-md bg-orange-400 min-h-12 text-center">
+         <Link
+             to="#"
+             
+          >
+            4
+         </Link>
+         </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center">
+      <Link
+             to="#"
+             
+          >
+            5
+         </Link>
+      </div>
+      <div  className="rounded-md bg-orange-400 min-h-12 text-center" >
+      <Link
+             to="#"
+             
+          >
+            6
+         </Link>
+      </div>
+
+     
+    </div>
+                   
+                    
+    </>
+
+    
   )
 }
-
 export default Home
+  
